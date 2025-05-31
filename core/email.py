624f73084a21c9b45a507044e3827afd3ff65bf7
Juan Pablo Ante <juan.pablo.ante@correounivalle.edu.co>
2025-05-31 00:46:00 -0500

    Email Template v2

--- core/email.py
+++ core/email.py
@@ -7,20 +7,14 @@ from django.contrib.sites.shortcuts import get_current_site
 
 class PasswordResetEmail(email.PasswordResetEmail):
     template_name = 'email/password_reset_email.html'
+    subject_template_name = 'email/password_reset_subject.txt'
     
     def get_context_data(self):
-        # Obtenemos el contexto base de Djoser
+        # Obtenemos el contexto base de Djoser que ya tiene todo configurado
         context = super().get_context_data()
         
-        # Agregamos variables personalizadas
-        user = context.get('user')
-        context.update({
-            'user': user,
-            'domain': context.get('domain'),
-            'protocol': context.get('protocol', 'https'),
-            'url': context.get('url'),
-            'from_email': settings.DEFAULT_FROM_EMAIL,
-        })
+        # Solo agregamos nuestras variables adicionales
+        context['from_email'] = settings.DEFAULT_FROM_EMAIL
         
         return context
 
